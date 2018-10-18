dccbe34753246b5dfe45681b85bf2d7cf67d381b
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2018-10-18 15:13:10 +0200

    openssl: v0.5.1
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- openssl/src/lib.rs
+++ openssl/src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/lapin-futures-openssl/0.5.0/")]
+#![doc(html_root_url = "https://docs.rs/lapin-futures-openssl/0.5.1/")]
 
 //! lapin-futures-openssl
 //!

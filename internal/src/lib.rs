b4250f7202c5ace7d0f60549a792c94dee2557c3
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2018-11-01 11:42:40 +0100

    internal: v0.1.1
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- internal/src/lib.rs
+++ internal/src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/lapin-futures-tls-internal/0.1.0/")]
+#![doc(html_root_url = "https://docs.rs/lapin-futures-tls-internal/0.1.1/")]
 
 //! lapin-futures-openssl
 //!

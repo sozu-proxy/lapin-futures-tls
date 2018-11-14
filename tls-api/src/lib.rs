bfd9d2c4ac7d0ca699d26c6f806be464e47bff17
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2018-11-15 00:19:34 +0100

    tls-api: v0.13.3
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- tls-api/src/lib.rs
+++ tls-api/src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/lapin-futures-tls-api/0.13.2/")]
+#![doc(html_root_url = "https://docs.rs/lapin-futures-tls-api/0.13.3/")]
 
 //! lapin-futures-tls-api
 //!

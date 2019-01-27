537bdd748bdbedb9e883895784adeff7aa4f82aa
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-01-27 13:29:29 +0100

    rustls: v0.18.0
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- rustls/src/lib.rs
+++ rustls/src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/lapin-futures-rustls/0.17.1/")]
+#![doc(html_root_url = "https://docs.rs/lapin-futures-rustls/0.18.0/")]
 
 //! lapin-futures-rustls
 //!

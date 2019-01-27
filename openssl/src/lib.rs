efc9574b1d1cd0aa8faf74c45b1732234de829b9
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-01-27 13:29:06 +0100

    openssl: v0.7.0
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- openssl/src/lib.rs
+++ openssl/src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/lapin-futures-openssl/0.6.1/")]
+#![doc(html_root_url = "https://docs.rs/lapin-futures-openssl/0.7.0/")]
 
 //! lapin-futures-openssl
 //!

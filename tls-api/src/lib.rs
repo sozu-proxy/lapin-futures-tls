a388133223ab9cbacc132a17d2909206905bb56a
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-02-01 16:01:30 +0100

    tls-api: v0.16.0

--- tls-api/src/lib.rs
+++ tls-api/src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/lapin-futures-tls-api/0.15.0/")]
+#![doc(html_root_url = "https://docs.rs/lapin-futures-tls-api/0.16.0/")]
 
 //! lapin-futures-tls-api
 //!

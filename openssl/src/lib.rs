e1ee04e9575dcce00cab9a1ae173887235bd3ff3
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-02-01 16:00:48 +0100

    openssl: v0.8.0

--- openssl/src/lib.rs
+++ openssl/src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/lapin-futures-openssl/0.7.0/")]
+#![doc(html_root_url = "https://docs.rs/lapin-futures-openssl/0.8.0/")]
 
 //! lapin-futures-openssl
 //!

f47e4373e4654ed321e4742c77b7cf699b96703d
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-03-03 21:28:55 +0100

    native-tls: v0.10.0
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- native-tls/src/lib.rs
+++ native-tls/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/lapin-futures-native-tls/0.9.0/")]
+#![doc(html_root_url = "https://docs.rs/lapin-futures-native-tls/0.10.0/")]
 
 //! lapin-futures-native-tls
 //!
